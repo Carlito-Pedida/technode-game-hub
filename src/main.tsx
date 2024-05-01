3bf0182a75680a8338f0a68d9382ead71301dd61
ionic-project <carlped417@gmail.com>
2024-05-01 10:59:57 -0500

    Implemented dark mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.tsx";
 import "./index.css";
+import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
