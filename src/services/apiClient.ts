db968d6db920c06d8f553a8e5ed20932e6a09375
ionic-project <carlped417@gmail.com>
2024-05-02 10:33:52 -0500

    Env Variable

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c2fa6859c6b741809a677ce7f7f4bf6b"
+    key: import.meta.env.VITE_GAME_HUB_API_KEY
   }
 });
