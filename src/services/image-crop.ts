098b9fb84cbc3eb435ec01f0638c9f47edd2c328
ionic-project <carlped417@gmail.com>
2024-05-02 19:13:25 -0500

    Handle no image error

--- src/services/image-crop.ts
+++ src/services/image-crop.ts
@@ -1,5 +1,7 @@
+import no_image_placeholder from "../assets/no_image_placeholder.webp";
+
 const croppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return no_image_placeholder;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
