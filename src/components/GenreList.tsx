59d1e0eafb1d696ae0bd0250f0cced9148917e36
ionic-project <carlped417@gmail.com>
2024-05-18 11:31:59 -0500

    react-query on genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -27,7 +27,7 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
         Genre
       </Heading>
       <List>
-        {data.map((genre) => (
+        {data?.results.map((genre) => (
           <ListItem paddingY="5px" key={genre.id}>
             <HStack>
               <Image
